304a17d81ba35c41b6cf6ffec895af96c45b87c4
Jeremy <jeremyneilson31@gmail.com>
2019-02-12 15:18:28 -0700

    aucona2 design angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderPrimaryComponent } from './header/header-primary/header-primary.component';
+import { HeaderSecondaryComponent } from './header/header-secondary/header-secondary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderPrimaryComponent,
+    HeaderSecondaryComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/header/header-secondary/header-secondary.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'app-header-secondary',
+  templateUrl: './header-secondary.component.html',
+  styleUrls: ['./header-secondary.component.scss']
+})
+export class HeaderSecondaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
